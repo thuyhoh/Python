8a550de03cd282c2f2b0b270eedff5260f358777
thuyh0h <trongthuynl2004@gmail.com>
2025-03-11 17:30:21 +0700

    KNN and SVM

---
+++ Computer-Vision/ML-DL/KNN_N_SVM_Image_Classification_Machine_Learning/KNN.py
@@ -0,0 +1,46 @@
+
+#import thư viện cần thiết
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from matplotlib.pyplot import show
+from imutils import paths
+import pickle
+
+from datasets.simpledatasetloader import SimpleDatasetLoader
+from preprocessing.simplepreprocessor import SimplePreprocessor
+
+
+print("[INFO] Nạp ảnh để train...")
+imagePaths = list(paths.list_images("datasets/animals"))
+
+
+sp = SimplePreprocessor(32, 32)
+sdl = SimpleDatasetLoader(preprocessors=[sp])
+(data, labels) = sdl.load(imagePaths, verbose=500)
+data = data.reshape((data.shape[0], 3072))
+
+print("[INFO] Dung lượng Ma trận đặc trưng: {:.1f}MB".format(data.nbytes / (1024 * 1000.0)))
+
+le = LabelEncoder()
+labels = le.fit_transform(labels)
+
+(trainData, testData, trainLabel, testLabel) = train_test_split(data, labels, test_size=0.25, random_state=42)
+
+print("[Đánh giá Bộ phân lớp k-NN ...]")
+model = KNeighborsClassifier(n_neighbors=3, n_jobs=-1)
+model.fit(trainData, trainLabel)
+
+# Lưu file model
+knnPickle = open('knnpickle_file.pkl', 'wb')
+pickle.dump(model, knnPickle)
+
+print(classification_report(testLabel, model.predict(testData), target_names=le.classes_))
+
+# Tạo và hiển thị ma trận confusion
+cm = confusion_matrix(testLabel, model.predict(testData))
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
+show()
